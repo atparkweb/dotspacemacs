1f199ab8667c742bd4dfd3532b448419b6589b1a
A Park <atpark333@gmail.com>
2023-01-24 23:14:28 +0900

    Add Ocaml layer and remove useless lines

--- init.el
+++ init.el
@@ -35,6 +35,7 @@ This function should only modify configuration layer settings."
    '(
      auto-completion
      common-lisp
+     csharp
      csv
      django
      docker
@@ -46,16 +47,18 @@ This function should only modify configuration layer settings."
           git-magit-status-fullscreen t)
      graphviz
      graphql
-     haskell
      helm
      (javascript :variables
                  javascript-backend 'tide
                  javascript-fmt-tool 'prettier
+                 javascript-fmt-on-save t
+                 js2-mode-show-strict-warnings nil
                  node-add-modules-path t)
      lsp
      lua
      markdown
      multiple-cursors
+     ocaml
      org
      phoenix
      php
@@ -64,7 +67,7 @@ This function should only modify configuration layer settings."
              python-backend 'python-lsp-server
              python-test-runner 'pytest)
      (react :variables
-            js-indent-level 2)
+            js-indent-level 4)
      ruby
      (scheme :variables
              scheme-implementation '(guile racket))
@@ -79,8 +82,9 @@ This function should only modify configuration layer settings."
      (typescript :variables
                  typescript-backend 'tide
                  typescript-fmt-tool 'prettier
-                 typescript-linter 'tslint
-                 tide-tsserver-executable "/home/atpark333/.nvm/versions/node/v16.16.0/bin/tsserver")
+                 typescript-fmt-on-save t
+                 js2-mode-show-strict-warnings nil
+                 typescript-linter 'tslint)
      vue
      (xclipboard :variables
                  xclipboard-enable-cliphist t)
@@ -590,7 +594,6 @@ See the header of this file for more information."
   (interactive)
   (tide-setup)
   (flycheck-mode +1)
-  (setq typescript-indent-level 2)
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
   (eldoc-mode +1)
   (tide-hl-identifier-mode +1)
@@ -610,7 +613,7 @@ If you are unsure, try setting them in `dotspacemacs/user-config' first."
         `((".*" . ,temporary-file-directory)))
   (setq auto-save-file-name-transforms
         `((".*" ,temporary-file-directory t)))
-  (setup-indent 2)
+  (setup-indent 4)
 )
 
 
@@ -632,13 +635,9 @@ before packages are loaded."
     (lambda ()
       (spacemacs/toggle-frame-fullscreen)
       (spacemacs/toggle-transparency))))
-  (add-hook 'window-setup-hook (spacemacs/toggle-fill-column-indicator))
   (add-hook 'before-save-hook 'tide-format-before-save)
   (add-hook 'typescript-mode-hook #'setup-tide)
   (spacemacs/toggle-evil-safe-lisp-structural-editing-on-register-hook-common-lisp-mode)
-  (setq helm-swoop-split-with-multiple-windows t)
-  (setq helm-swoop-split-direction 'split-window-horizontally)
-
   (define-key evil-normal-state-map (kbd "-")
     (lambda ()
       (interactive)
@@ -668,9 +667,9 @@ This function is called at the very end of Spacemacs initialization."
  '(custom-safe-themes
    '("a3e99dbdaa138996bb0c9c806bc3c3c6b4fd61d6973b946d750b555af8b7555b" "db7f422324a763cfdea47abf0f931461d1493f2ecf8b42be87bbbbbabf287bfe" "78e6be576f4a526d212d5f9a8798e5706990216e9be10174e3f3b015b8662e27" "a37d20710ab581792b7c9f8a075fcbb775d4ffa6c8bce9137c84951b1b453016" "eb122e1df607ee9364c2dfb118ae4715a49f1a9e070b9d2eb033f1cefd50a908" default))
  '(org-agenda-files
-   '("/home/atpark333/Documents/planner/curriculum.org" "/home/atpark333/Documents/planner/engineering.org"))
+   '("~/Documents/planner/tasks.org" "/home/atpark333/Documents/planner/curriculum.org" "/home/atpark333/Documents/planner/engineering.org"))
  '(package-selected-packages
-   '(company-php ac-php-core xcscope company-phpactor drupal-mode geben php-auto-yasnippets php-extras php-mode phpactor composer php-runtime phpcbf phpunit graphviz-dot-mode add-node-modules-path company counsel-gtags counsel swiper ivy dap-mode lsp-docker lsp-treemacs bui yaml lsp-mode markdown-mode ggtags helm-gtags impatient-mode htmlize import-js grizzl js-doc js2-refactor yasnippet multiple-cursors livid-mode nodejs-repl npm-mode prettier-js skewer-mode js2-mode simple-httpd tern web-beautify ws-butler writeroom-mode winum which-key volatile-highlights vim-powerline vi-tilde-fringe uuidgen use-package undo-tree treemacs-projectile treemacs-persp treemacs-icons-dired treemacs-evil toc-org term-cursor symon symbol-overlay string-inflection string-edit spacemacs-whitespace-cleanup spacemacs-purpose-popwin spaceline-all-the-icons space-doc restart-emacs request rainbow-delimiters quickrun popwin pcre2el password-generator paradox overseer org-superstar open-junk-file nameless multi-line macrostep lorem-ipsum link-hint inspector info+ indent-guide hybrid-mode hungry-delete holy-mode hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-xref helm-themes helm-swoop helm-purpose helm-projectile helm-org helm-mode-manager helm-make helm-flx helm-descbinds helm-ag google-translate golden-ratio font-lock+ flycheck-package flycheck-elsa flx-ido fancy-battery eyebrowse expand-region evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-textobj-line evil-surround evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-goggles evil-exchange evil-evilified-state evil-escape evil-ediff evil-easymotion evil-collection evil-cleverparens evil-args evil-anzu eval-sexp-fu emr elisp-slime-nav elisp-def editorconfig dumb-jump drag-stuff dotenv-mode dired-quick-sort diminish devdocs define-word column-enforce-mode clean-aindent-mode centered-cursor-mode auto-highlight-symbol auto-compile aggressive-indent ace-link ace-jump-helm-line))
+   '(omnisharp company-php ac-php-core xcscope company-phpactor drupal-mode geben php-auto-yasnippets php-extras php-mode phpactor composer php-runtime phpcbf phpunit graphviz-dot-mode add-node-modules-path company counsel-gtags counsel swiper ivy dap-mode lsp-docker lsp-treemacs bui yaml lsp-mode markdown-mode ggtags helm-gtags impatient-mode htmlize import-js grizzl js-doc js2-refactor yasnippet multiple-cursors livid-mode nodejs-repl npm-mode prettier-js skewer-mode js2-mode simple-httpd tern web-beautify ws-butler writeroom-mode winum which-key volatile-highlights vim-powerline vi-tilde-fringe uuidgen use-package undo-tree treemacs-projectile treemacs-persp treemacs-icons-dired treemacs-evil toc-org term-cursor symon symbol-overlay string-inflection string-edit spacemacs-whitespace-cleanup spacemacs-purpose-popwin spaceline-all-the-icons space-doc restart-emacs request rainbow-delimiters quickrun popwin pcre2el password-generator paradox overseer org-superstar open-junk-file nameless multi-line macrostep lorem-ipsum link-hint inspector info+ indent-guide hybrid-mode hungry-delete holy-mode hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-xref helm-themes helm-swoop helm-purpose helm-projectile helm-org helm-mode-manager helm-make helm-flx helm-descbinds helm-ag google-translate golden-ratio font-lock+ flycheck-package flycheck-elsa flx-ido fancy-battery eyebrowse expand-region evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-textobj-line evil-surround evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-goggles evil-exchange evil-evilified-state evil-escape evil-ediff evil-easymotion evil-collection evil-cleverparens evil-args evil-anzu eval-sexp-fu emr elisp-slime-nav elisp-def editorconfig dumb-jump drag-stuff dotenv-mode dired-quick-sort diminish devdocs define-word column-enforce-mode clean-aindent-mode centered-cursor-mode auto-highlight-symbol auto-compile aggressive-indent ace-link ace-jump-helm-line))
  '(warning-suppress-log-types '((comp) (comp)))
  '(warning-suppress-types '((comp))))
 (custom-set-faces
@@ -678,6 +677,5 @@ This function is called at the very end of Spacemacs initialization."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background nil))))
  '(highlight-parentheses-highlight ((nil (:weight ultra-bold))) t))
 )
