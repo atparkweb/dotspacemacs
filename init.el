64b41dc15e2747461de20ce6d038712b32ce9e94
Andy Park <atpark333@gmail.com>
2024-03-13 11:24:29 +0900

    Add Vimscript layer

--- init.el
+++ init.el
@@ -32,7 +32,8 @@ This function should only modify configuration layer settings."
 
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
-   '(go
+   '(vimscript
+     go
      auto-completion
      (c-c++ :variables
             c-c++-backend 'lsp-clangd
