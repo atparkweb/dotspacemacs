a194c277daf59b58f6accbbd6f118927980c9a33
Andy Park <atpark333@gmail.com>
2025-06-09 10:32:32 +0900

    Update font
    The M Plus font name has changed on my system.

--- init.el
+++ init.el
@@ -290,7 +290,7 @@ It should only modify the values of Spacemacs settings."
    ;; Default font or prioritized list of fonts. The `:size' can be specified as
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
-   dotspacemacs-default-font '("M PLUS 1 Code"
+   dotspacemacs-default-font '("M PLUS Code Latin Expanded"
 							   :size 14
 							   :weight regular
 							   :width normal)
