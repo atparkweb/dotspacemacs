dcb4965dd89652eae6efb49ede531acc86a08813
Andy Park <atpark333@gmail.com>
2025-06-17 15:03:21 +0900

    Organize and format tide-setup

--- init.el
+++ init.el
@@ -589,12 +589,12 @@ See the header of this file for more information."
 (defun setup-tide ()
   (interactive)
   (tide-setup)
-  (flycheck-mode +1)
-  (setq typescript-indent-level 4)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
+  (eldoc-mode t)
+  (flycheck-mode t)
+  (company-mode t)
   (tide-hl-identifier-mode +1)
-  (company-mode +1))
+  (setq typescript-indent-level 4)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled)))
 
 (setq company-tooltip-align-annotations t)
 
